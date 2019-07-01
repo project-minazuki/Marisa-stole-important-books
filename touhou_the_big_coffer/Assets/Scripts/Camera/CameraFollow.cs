40c9bfb4098bb9e4e2c35d0b15528eb52c9a517d
Tmc <494521535@qq.com>
2019-07-01 17:25:26 +0800

    test0.0.1

---
+++ touhou_the_big_coffer/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position += new Vector3(3, 0, 0), 2*Time.deltaTime);
+    }
+}
---
+++ touhou_the_big_coffer/Assets/Scripts/Player/PlayerJumping.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumping : MonoBehaviour
+{
+
+    PlayerMoving playerMoving;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMoving = GetComponent<PlayerMoving>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//#if UNITY_STANDALONE || UNITY_EDITOR    //if the current platform is not mobile, setting mouse handling 
+
+        if (Input.GetButton("Fire1") && playerMoving.isPlayerJumped == false)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0,8);
+            playerMoving.isPlayerJumped = true;
+        }
+
+//#endif
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "RefleshJump" && playerMoving.isPlayerJumped == true)
+        {
+            playerMoving.isPlayerJumped = false;
+        }
+    }
+}
