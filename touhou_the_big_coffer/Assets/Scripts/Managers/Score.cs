34ebd1795976dcae2eb421b3d140fbfdb027b463
Tmc <494521535@qq.com>
2019-07-03 11:59:25 +0800

    test0.0.6

---
+++ touhou_the_big_coffer/Assets/Scripts/Managers/Score.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int BasicScore;
+    public int ExtraScore;
+    public int score;
+    public Text scoreText;
+    public int test;
+    public GameObject player;
+    public float positionX;
+    void Start()
+    {
+        score = 0;
+        BasicScore = 0;
+        ExtraScore = 0;
+        test = 0;
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test = 78685;
+        if (player != null)
+        {
+            BasicScore = (int)player.transform.position.x;
+            test = 999;
+        }
+        else
+        {
+            scoreText.text = "滑稽";
+            test = 123;
+        }
+        score = BasicScore + ExtraScore;
+        UpdateScore();
+    }
+    public void AddScore(int newScoreValue)
+    {
+        ExtraScore += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText.text= "得分：" + score;
+       
+    }
+}
