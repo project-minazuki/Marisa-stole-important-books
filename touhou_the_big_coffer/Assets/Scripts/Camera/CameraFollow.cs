041e246ff496e925671331250a07033d5f184036
Tmc <494521535@qq.com>
2019-07-04 15:27:47 +0800

    Test0.0.8

--- touhou_the_big_coffer/Assets/Scripts/Camera/CameraFollow.cs
+++ touhou_the_big_coffer/Assets/Scripts/Camera/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private GameObject player;
+    private float zoomInNum = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,33 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x + 4f, 0, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x + 4f, transform.position.y, transform.position.z);
+    }
+
+    public void zoomIn()
+    {
+        if (zoomInNum < 11)
+        {
+            zoomInNum += 1;
+            transform.position = new Vector3(transform.position.x, transform.position.y + player.transform.position.y / 10 * (2.1f - zoomInNum/5) + player.GetComponent<Rigidbody2D>().velocity.y/55, transform.position.z + 0.5f * (2.1f - zoomInNum / 5));
+        }
+        else
+        {
+            zoomInNum = 11;
+            transform.position = new Vector3(transform.position.x, transform.position.y, -5);
+        }
+    }
+    public void zoomOut()
+    {
+        if (zoomInNum > 0)
+        {
+            zoomInNum -= 1;
+            transform.position = new Vector3(transform.position.x, transform.position.y / 1.3f, transform.position.z - 0.5f * (zoomInNum / 5 - 0.1f));
+        }
+        else
+        {
+            zoomInNum = 0;
+            transform.position = new Vector3(transform.position.x, transform.position.y/1.2f, -10);
+        }
     }
 }
